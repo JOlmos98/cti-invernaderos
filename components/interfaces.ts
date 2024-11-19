1eb39252927b04c266f367b539cb6f631be154b7
JOlmos98 <soler98@hotmail.es>
2024-11-19 16:21:03 +0100

    ESLint fixed.

--- components/interfaces.ts
+++ components/interfaces.ts
@@ -3,7 +3,7 @@ import { IconType } from "react-icons";
 
 
 export interface MenuElemento {
-    etiqueta: String; 
+    etiqueta: string; 
     icono: IconType; 
     ruta: string ; // propiedad de la ruta, para poder movernos 
 }
